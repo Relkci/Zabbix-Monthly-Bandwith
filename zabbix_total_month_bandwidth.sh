3163e67d40157808745fd128a98f00c2242046ad
Kent Ickler <kent@relkci.com>
2021-07-05 23:31:27 -0600

    Update zabbix_total_month_bandwidth.sh
    added Byte handler, added interface argument

--- zabbix_total_month_bandwidth.sh
+++ zabbix_total_month_bandwidth.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
     # Current month total bandwidth in MB
 
-    i=$(vnstat --oneline | awk -F\; '{ print $11 }')
+    int=$1
+    i=$(vnstat -i $int --oneline  | awk -F\; '{ print $11 }')
 
     bandwidth_number=$(echo $i | awk '{ print $1 }')
     bandwidth_unit=$(echo $i | awk '{ print $2 }')
 
     case "$bandwidth_unit" in
+    B)    bandwidth_number_MB=$(echo "$bandwidth_number/1024/1024" | bc)
+        ;;
     KiB)    bandwidth_number_MB=$(echo "$bandwidth_number/1024" | bc)
         ;;
     MiB)    bandwidth_number_MB=$bandwidth_number
@@ -18,3 +21,4 @@
     esac
 
     echo $bandwidth_number_MB
+
